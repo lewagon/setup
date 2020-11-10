cf4ed99973ec73bb6645195404455e08c951123c
Benjamin Baranger <baranger.benjamin@gmail.com>
2020-11-10 16:13:53 +0100

    update script to not duplicate dotfile info

--- utils/install_node.sh
+++ utils/install_node.sh
@@ -2,8 +2,5 @@
 
 cd ~
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
-echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> ~/.zshrc
-echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> ~/.zshrc
 source ~/.zshrc
 nvm install 14.0.0
